43d3a4820bcbc241f8742b80e79f5d071f4b9563
Ludo <Kiluhasenpai@gmail.com>
2020-01-07 09:53:45 +0100

    Modele retard class

---
+++ Juste-IFI-toi/Models/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Juste_IFI_toi.Models
+{
+    public class Retard
+    {
+        public int Id { get; set; }
+        public DateTime date { get; set; }
+        public String motif { get; set; }
+        public String photo { get; set; }
+    }
+}
